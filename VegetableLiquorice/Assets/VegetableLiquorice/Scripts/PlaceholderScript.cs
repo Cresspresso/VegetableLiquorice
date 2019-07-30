ca323551695d3ad93782ba9509dcc8580320433d
Elijah Shadbolt <elijah.shadbolt@gmail.com>
2019-07-31 11:01:31 +1200

    Unity Project
    + Unity Project
    + Standard Assets
    + .gitignore

---
+++ VegetableLiquorice/Assets/VegetableLiquorice/Scripts/PlaceholderScript.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceholderScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
