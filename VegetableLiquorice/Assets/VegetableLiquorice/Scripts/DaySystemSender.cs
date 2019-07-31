279049da853d006b59636138ccc0eddc9a033dcf
Elijah Shadbolt <elijah.shadbolt@gmail.com>
2019-07-31 15:04:34 +1200

    DaySystem Days and Game Restart
    + DaySystem object persistence when moving between scenes.
    + Increments the day when button in ElijahScene2 is pressed.
    + Resets the current day to 0 when button in ElijahEndScene is pressed.

---
+++ VegetableLiquorice/Assets/VegetableLiquorice/Scripts/DaySystemSender.cs
@@ -0,0 +1,17 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DaySystemSender : MonoBehaviour
+{
+	public void NextDay()
+	{
+		DaySystem.instance.NextDay();
+	}
+
+	public void ResetGame()
+	{
+		DaySystem.instance.ResetGame();
+	}
+}
