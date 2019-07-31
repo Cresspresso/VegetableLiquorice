b344b96f44d404b63970b86180b77a9f54614001
CSymphony <32253069+CSymphony@users.noreply.github.com>
2019-07-31 12:31:20 +1200

    Scene Stuff

---
+++ VegetableLiquorice/Assets/VegetableLiquorice/Scripts/CubeScalar.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeScalar : MonoBehaviour {
+
+    public Vector3 maxScale = new Vector3(5f, 5f, 5f);
+    public float ScaleFactor = 1.2f;
+
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 currentScale = this.transform.localScale;
+        currentScale = currentScale + Vector3.one * ScaleFactor * Time.deltaTime;
+        if (currentScale.x > maxScale.x
+            || currentScale.y > maxScale.y
+            || currentScale.z > maxScale.z)
+        {
+            currentScale = maxScale;
+        }
+        this.transform.localScale = currentScale;
+    }
+}
---
+++ VegetableLiquorice/Assets/VegetableLiquorice/Scripts/SceneLoader.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class SceneLoader : MonoBehaviour
+{
+    public string SceneName = "VegetableLiquoriceScene";
+
+
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(SceneName);
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
