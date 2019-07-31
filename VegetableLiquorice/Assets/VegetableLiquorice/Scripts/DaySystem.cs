673c7283c01b744fe58e7e49807a4dc24fae6452
Elijah Shadbolt <elijah.shadbolt@gmail.com>
2019-07-31 14:35:24 +1200

    DaySystem initial commit

---
+++ VegetableLiquorice/Assets/VegetableLiquorice/Scripts/DaySystem.cs
@@ -0,0 +1,62 @@
+﻿
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DaySystem : MonoBehaviour
+{
+	public int numDays = 7;
+	public int currentDay = 0;
+
+
+
+	#region Singleton
+	private static DaySystem _instance;
+	public static DaySystem instance
+	{
+		get
+		{
+			if (!_instance)
+			{
+				_instance = FindObjectOfType<DaySystem>();
+				if (!_instance)
+				{
+					Debug.LogError("DaySystem.instance is null");
+				}
+			}
+			return _instance;
+		}
+		private set
+		{
+			if (_instance)
+			{
+				_instance = value;
+			}
+			else
+			{
+				Debug.LogError("DaySystem.instance cannot be null");
+			}
+		}
+	}
+	#endregion Singleton
+
+
+
+	private void Awake()
+	{
+		#region Singleton
+		if (instance != this)
+		{
+			Destroy(gameObject);
+			return;
+		}
+		DontDestroyOnLoad(gameObject);
+		#endregion Singleton
+	}
+
+	private void Start()
+	{
+		Debug.Log("Wow");
+	}
+}
