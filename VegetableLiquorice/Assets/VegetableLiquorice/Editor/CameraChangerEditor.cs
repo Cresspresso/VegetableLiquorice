ce21e53f4053e9a988afd031832d676d89d5c67d
Elijah Shadbolt <elijah.shadbolt@gmail.com>
2019-08-07 11:14:13 +1200

    added context menu item for camera changer
    In the inspector you can click the cog icon on the CameraChanger component and choose Apply Viewpoint to show the appropriate viewpoint objects.

---
+++ VegetableLiquorice/Assets/VegetableLiquorice/Editor/CameraChangerEditor.cs
@@ -0,0 +1,23 @@
+﻿
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class CameraChangerEditor : Editor
+{
+    [MenuItem("CONTEXT/CameraChanger/Apply Viewpoint")]
+    static void ApplyViewpoint(MenuCommand command)
+    {
+        CameraChanger cc = (CameraChanger)command.context;
+
+        var objectsToSave = new List<Object>();
+        foreach (var v in cc.viewpoints)
+        {
+            objectsToSave.AddRange(v.objects);
+        }
+        var oj = objectsToSave.ToArray();
+        Undo.RecordObjects(oj, "Apply Viewpoint");
+
+        cc.ApplyActive();
+    }
+}
