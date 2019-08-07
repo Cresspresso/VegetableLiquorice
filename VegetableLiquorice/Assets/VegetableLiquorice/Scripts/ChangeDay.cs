759f745a6495434bfcf50cfbaba5d5a0ae93a0b4
CSymphony <32253069+CSymphony@users.noreply.github.com>
2019-08-07 17:19:56 +1200

    Scene Fade

---
+++ VegetableLiquorice/Assets/VegetableLiquorice/Scripts/ChangeDay.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeDay : MonoBehaviour {
+
+    public Animator animator;
+
+    private int levelToLoad;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update () {
+		/*if (Input.GetMouseButtonDown(0))
+        {
+            FadeToLevel(0);
+        }*/
+	}
+
+    public void FadeToLevel (int levelIndex)
+    {
+        levelToLoad = levelIndex;
+        animator.SetTrigger("FadeOut");
+    }
+
+    public void OnFadeComplete ()
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+}
